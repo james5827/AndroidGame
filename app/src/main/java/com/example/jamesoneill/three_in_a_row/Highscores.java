0d737293e9cc3e7abef871341f640b05818ab522
James O'Neill <jamesoneill5827@gmail.com>
2018-06-22 08:37:25 +1000

    It might be working now

--- app/src/main/java/com/example/jamesoneill/three_in_a_row/Highscores.java
+++ app/src/main/java/com/example/jamesoneill/three_in_a_row/Highscores.java
@@ -1,5 +1,6 @@
 package com.example.jamesoneill.three_in_a_row;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,6 +12,10 @@ import android.view.View;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.github.amlcurran.showcaseview.ShowcaseView;
+import com.github.amlcurran.showcaseview.targets.Target;
+import com.github.amlcurran.showcaseview.targets.ViewTarget;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +34,10 @@ public class Highscores extends AppCompatActivity {
 
         this.difficultySpinner = findViewById(R.id.highScoreSpinner);
         initialiseGridSizeSeekBar();
+
+        if(getIntent().hasExtra(Config.TUTORIAL)){
+            tutorialShowCaseView();
+        }
     }
 
     public void displayScores(View view){
@@ -70,5 +79,63 @@ public class Highscores extends AppCompatActivity {
 
         this.gridSizeSeek.setProgress(Config.getColNumbers() -4);
         gridViewlbl.setText(String.format(getString(R.string.grid_size_seek_bar), Config.getColNumbers()));
+    }
+
+    private void tutorialShowCaseView(){
+        new Highscores.scvHighScoresTutorial(this);
+    }
+
+    private class scvHighScoresTutorial implements View.OnClickListener{
+        private byte counter;
+        private ShowcaseView scv;
+        private Activity context;
+
+        private scvHighScoresTutorial(Activity context) {
+            this.counter = 0;
+            this.context = context;
+            this.scv = new ShowcaseView.Builder(context)
+                    .withNewStyleShowcase()
+                    .setStyle(R.style.CustomShowcaseTheme)
+                    .blockAllTouches()
+                    .setTarget(Target.NONE)
+                    .setContentTitle("This is the Home Screen")
+                    .setContentText("From here you can navigate around the rest of the application")
+                    .setOnClickListener(this)
+                    .build();
+
+            scv.setButtonText("Next");
+        }
+
+        @Override
+        public void onClick(View view) {
+            switch (counter) {
+                case 0:
+                    scv.setShowcase(new ViewTarget(R.id.btn_settings, context), false);
+                    scv.setContentTitle("Settings Button");
+                    scv.setContentText("This button will take you to the settings screen, where you can change game settings.");
+                    break;
+                case 1:
+                    scv.setShowcase(new ViewTarget(R.id.btn_highscores, context), true);
+                    scv.setContentTitle("High Scores Button");
+                    scv.setContentText("This button will take you to the high score screen, where you can see your highscores.");
+                    break;
+                case 2:
+                    scv.setShowcase(new ViewTarget(R.id.btn_help, context), true);
+                    scv.setContentTitle("Help Button");
+                    scv.setContentText("This button will take you to the help screen, you can access various tutorials here.");
+                    scv.setButtonPosition(Config.getBottomLeftParams(context));
+                    break;
+                case 3:
+                    scv.setShowcase(new ViewTarget(R.id.btn_play, context), true);
+                    scv.setContentTitle("Play");
+                    scv.setContentText("Swiping will take you to the game screen.");
+                    break;
+                case 4:
+                    scv.hide();
+                    Config.createShowCaseIntent(Highscores.this, Play.class);
+                    break;
+            }
+            ++counter;
+        }
     }
 }
